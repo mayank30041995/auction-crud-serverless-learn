4e5b24329b07d445d615ad04b4ef0326ae42134f
Mayank Sharma <mayankece155@gmail.com>
2026-05-28 20:14:08 +0530

    create auction setup dynamodb

---
+++ src/handlers/createAuction.mjs
@@ -0,0 +1,22 @@
+async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+  };
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = createAuction;
+
+// sls deploy function --function createAuction
+
+// git clone https://github.com/codingly-io/sls-base.git auction-service
+// rmdir /s /q .git  // remove git  init folder
