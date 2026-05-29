f5e6c26665d929066950527651cc91b940f9beb7
Mayank Sharma <mayankece155@gmail.com>
2026-05-29 20:43:59 +0530

    Implement common middleware

--- src/handlers/getAuctions.mjs
+++ src/handlers/getAuctions.mjs
@@ -1,8 +1,6 @@
 import AWS from "aws-sdk";
 import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpEventNormalizer from "@middy/http-event-normalizer";
-import httpErrorHandler from "@middy/http-error-handler";
+import commonMiddleware from "../lib/commonMiddleware.mjs";
 import createError from "http-errors";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -29,9 +27,4 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = middy(getAuctions)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
-
-
+export const handler = commonMiddleware(getAuctions);
