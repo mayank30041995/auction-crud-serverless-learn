a478cf3ab59b355faf496d665b16b030a7743d87
Mayank Sharma <mayankece155@gmail.com>
2026-05-30 10:20:57 +0530

    Closing auctions

--- src/handlers/processAuctions.mjs
+++ src/handlers/processAuctions.mjs
@@ -1,8 +1,19 @@
+import createError from "http-errors";
+import { closeAuction } from "../lib/closeAuction.mjs";
 import { getEndedAuctions } from "../lib/getEndedAuctions.mjs";
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions();
-  console.log(auctionsToClose);
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map((auction) =>
+      closeAuction(auction),
+    );
+    await Promise.all(closePromises);
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
