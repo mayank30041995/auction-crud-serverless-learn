e2df7be619cd3c22b71616955a409bcef01b083b
Mayank Sharma <mayankece155@gmail.com>
2026-05-30 10:03:39 +0530

    Identify auctions that need to be closed

--- src/handlers/processAuctions.mjs
+++ src/handlers/processAuctions.mjs
@@ -1,5 +1,10 @@
+import { getEndedAuctions } from "../lib/getEndedAuctions.mjs";
+
 async function processAuctions(event, context) {
-  console.log("processing auctions!");
+  const auctionsToClose = await getEndedAuctions();
+  console.log(auctionsToClose);
 }
 
 export const handler = processAuctions;
+
+// sls invoke -f processAuctions -l
